6f7285d21caceff6085408ac6c83535386a3241e
dharani-selvan <143258033+dharani-selvan@users.noreply.github.com>
2024-10-08 20:11:49 +0530

    Add files via upload

---
+++ java/Greatest3.java
@@ -0,0 +1,19 @@
+public class Greatest3 {
+    public static void main(String[] args) {
+        
+    
+    int a=3,b=9,c=99;
+    if(a>b && a>c)
+    {
+        System.out.println(a);
+    }
+    else if(b>c)
+    {
+        System.out.println(b);
+    }
+    else
+    {
+        System.out.println(c);
+    }
+}
+}
---
+++ java/Oddeven.java
@@ -0,0 +1,13 @@
+public class Oddeven {
+    public static void main(String[] args) {
+        int n=6;
+        if(n%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
